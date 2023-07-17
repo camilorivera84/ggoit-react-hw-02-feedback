1923679a5b176bf4a31c8a37298ac668984e135e
camilorivera84 <110140389+camilorivera84@users.noreply.github.com>
2023-07-16 20:36:22 -0500

    ajustes tarea

--- src/components/FeedbackWidget/FeedbackWidget.js
+++ src/components/FeedbackWidget/FeedbackWidget.js
@@ -1,11 +1,77 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+`;
+
+const WidgetContainer = styled.div`
+  text-align: center;
+`;
+
+const FeedbackButton = styled.button`
+  background: linear-gradient(to bottom right, #ff4d00, #800000);
+  color: white;
+  border-radius: 5px;
+  padding: 10px 20px;
+  margin: 5px;
+  cursor: pointer;
+  border: none;
+  transition: box-shadow 0.3s;
+
+  &:hover {
+    box-shadow: 0 0 5px #ff5900, 0 0 10px #ff6a00, 0 0 15px #ff5900;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const ResultsContainer = styled.div`
+  margin: auto 0;
+`;
+const NoFeedbackContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+`;
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <ResultsContainer>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <p>Total feedback: {total}</p>
+    <p>Positive feedback percentage: {positivePercentage}%</p>
+  </ResultsContainer>
+);
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <WidgetContainer>
+    {options.map(option => (
+      <FeedbackButton key={option} onClick={() => onLeaveFeedback(option)}>
+        {option}
+      </FeedbackButton>
+    ))}
+  </WidgetContainer>
+);
+
+const Section = ({ title, children }) => (
+  <div>
+    <h2>{title}</h2>
+    {children}
+  </div>
+);
 
 const FeedbackWidget = () => {
-  const [selected, setSelected] = useState(null);
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
   const handleButtonClick = feedbackType => {
-    setSelected(feedbackType);
     setFeedback(prevFeedback => ({
       ...prevFeedback,
       [feedbackType]: prevFeedback[feedbackType] + 1,
@@ -23,51 +89,31 @@ const FeedbackWidget = () => {
     return total > 0 ? Math.round((good / total) * 100) : 0;
   };
 
-  const renderResults = () => {
-    if (selected === null) {
-      return <p>No feedback given</p>;
-    }
-
-    const { good, neutral, bad } = feedback;
-    const total = countTotalFeedback();
-    const positivePercentage = countPositiveFeedbackPercentage();
-
-    return (
-      <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Total feedback: {total}</p>
-        <p>Positive feedback percentage: {positivePercentage}%</p>
-      </div>
-    );
-  };
-
   return (
-    <div className="feedback-widget">
-      <h2>Feedback Widget</h2>
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleButtonClick}
+        />
+      </Section>
 
-      <button
-        className="feedback-button"
-        onClick={() => handleButtonClick('good')}
-      >
-        Good
-      </button>
-      <button
-        className="feedback-button"
-        onClick={() => handleButtonClick('neutral')}
-      >
-        Neutral
-      </button>
-      <button
-        className="feedback-button"
-        onClick={() => handleButtonClick('bad')}
-      >
-        Bad
-      </button>
-
-      {renderResults()}
-    </div>
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
+          <NoFeedbackContainer>
+            <p>No feedback given</p>
+          </NoFeedbackContainer>
+        ) : (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        )}
+      </Section>
+    </Container>
   );
 };
 
